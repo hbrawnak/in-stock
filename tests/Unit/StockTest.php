23200ece1bb7d1cc9b65eed6301a717e60d5fbad
Habibur Rahman <hbrawnak@gmail.com>
2020-07-25 20:43:19 +0600

    test update local db after being tracked

--- tests/Unit/StockTest.php
+++ tests/Unit/StockTest.php
@@ -2,7 +2,10 @@
 
 namespace Tests\Unit;
 
+use App\Clients\Client;
 use App\Clients\ClientException;
+use App\Clients\StockStatus;
+use Facades\App\Clients\ClientFactory;
 use App\Retailer;
 use App\Stock;
 use Illuminate\Foundation\Testing\RefreshDatabase;
@@ -25,5 +28,25 @@ class StockTest extends TestCase
         $this->expectException(ClientException::class);
 
         Stock::first()->track();
+    }
+
+    /**
+     * @test
+     */
+    public function it_updates_local_stock_status_after_being_tracked()
+    {
+        $this->seed(RetailerWithProduct::class);
+
+        ClientFactory::shouldReceive('make')->andReturn(new class implements Client {
+                public function checkAvailability(Stock $stock): StockStatus
+                {
+                    return new StockStatus($available = true, $price = 1000);
+                }
+            }
+        );
+
+        $stock = tap(Stock::first())->track();
+        $this->assertTrue($stock->in_stock);
+        $this->assertEquals(1000, $stock->price);
     }
 }
